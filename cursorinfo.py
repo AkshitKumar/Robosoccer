87cd2fc20eb19b24668c379fe621e55a9cbe17bd
AkshitKumar <akshitkumar100@gmail.com>
2015-05-26 15:31:22 +0530

    Added the Serial Communication Code

--- cursorinfo.py
+++ cursorinfo.py
@@ -1,5 +1,9 @@
 import pygame
-
+import serial
+serPort = '/dev/tty.usbmodem6bbd7002'
+baudRate = 9600
+ser = serial.Serial(serPort,baudRate)
+ser.open()
 pygame.init()
 
 gameDisplay = pygame.display.set_mode((800,600))
@@ -8,9 +12,19 @@ pygame.display.set_caption("Cursor Information")
 pygame.display.update()
 
 while 1:
-    for event in pygame.event.get():
-        mousex,mousey = event.pos
-        print mousex,mousey
+	try:
+    	for event in pygame.event.get():
+    	    mousex,mousey = event.pos
+    	    mousex = str(chr(mousex))
+    	    mousey = str(chr(mousey))
+    	    ser.write(mousex)
+    	    a = ser.readline()
+    	    print a
+    	    ser.write(mousey)
+    	    b = ser.readline()
+    	    print b
+    except:
+    	ser.close()
 
 pygame.quit()
 quit()
